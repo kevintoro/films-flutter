bc3b0fedc4f0673adf27bbd37d2436c7b8a4b007
Kevin Toro <kevinandrestg7@gmail.com>
2021-02-23 15:25:31 -0500

    remove test data, add media queries to responsive design, show data image

--- lib/src/widgets/card_swiper_widget.dart
+++ lib/src/widgets/card_swiper_widget.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import 'package:flutter_swiper/flutter_swiper.dart';
+import 'package:peliculas/src/models/film_model.dart';
 
 class CardSwiper extends StatelessWidget {
   const CardSwiper({
@@ -8,22 +9,30 @@ class CardSwiper extends StatelessWidget {
     @required this.films,
   }) : super(key: key);
 
-  final List<dynamic> films;
+  final List<Film> films;
 
   @override
   Widget build(BuildContext context) {
+    //Tamaño de la pantalla del dispositivo
+    final _screenSize = MediaQuery.of(context).size;
+
     return Container(
       padding: EdgeInsets.only(top: 15.0),
-      width: double.infinity,
-      height: 300.0,
       child: Swiper(
-        itemCount: 3,
+        itemWidth: _screenSize.width * 0.7,
+        itemHeight: _screenSize.height * 0.5,
+        itemCount: films.length,
         layout: SwiperLayout.STACK,
-        itemWidth: 200.0,
         itemBuilder: (context, index) {
-          return Image.network(
-            'http://via.placeholder.com/350x150',
-            fit: BoxFit.fill,
+          return ClipRRect(
+            borderRadius: BorderRadius.circular(20.0),
+            child: FadeInImage(
+              placeholder: AssetImage('assets/img/no-image.jpg'),
+              image: NetworkImage(
+                films[index].getPosterPath(),
+              ),
+              fit: BoxFit.cover,
+            ),
           );
         },
       ),
