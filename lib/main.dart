e34a005f4f7de64137ee05e360e63e331c2b1c6f
Kevin Toro <kevinandrestg7@gmail.com>
2021-02-27 01:15:42 -0500

    add film details route

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:peliculas/src/pages/film_detail.dart';
 
 import 'package:peliculas/src/pages/home_page.dart';
 
@@ -13,6 +14,7 @@ class MyApp extends StatelessWidget {
       initialRoute: '/',
       routes: {
         '/': (BuildContext context) => HomePage(),
+        'details': (BuildContext context) => FilmDetail(),
       },
     );
   }
